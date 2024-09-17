aca6fc37dbf162406864e58edb0c5e61c55da878
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2024-09-17 13:57:22 -0300

    set req delete in database

--- src/database.js
+++ src/database.js
@@ -2,8 +2,6 @@ import fs from 'node:fs/promises'
 
 const databasePath = new URL('../db.json', import.meta.url)
 
-console.log(databasePath)
-
 
 export class Database {
     #database = {}
@@ -38,6 +36,16 @@ export class Database {
 
         return data;
 
+    }
+
+
+    delete(table, id){
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+        
+        if(rowIndex > -1){
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
     }
 
 }
